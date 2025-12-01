1c325c6c1b6d9bcb36103bbec1b9880878c6f911
Bernard Jollans <berjollans@gmail.com>
2025-12-01 23:03:24 +0100

    Solve puzzle 1-2

--- src/Day1PasswordGetter.kt
+++ src/Day1PasswordGetter.kt
@@ -3,8 +3,9 @@ class Day1PasswordGetter : Runnable {
         val dial = Dial(size = 100, importantLocation = 0, initialPos = 50)
         val rotations = getLinesForDay("01", false)
         for (rotation in rotations) {
-            dial.rotate(rotation)
+            dial.rotateSlowly(rotation)
         }
         println("${dial.importantLocation} was hit ${dial.importantLocationMeets} times.")
+        println("${dial.importantLocation} was passed ${dial.importantLocationPasses} times.")
     }
 }
--- src/Dial.kt
+++ src/Dial.kt
@@ -1,13 +1,42 @@
 class Dial(val size: Int, val importantLocation: Int, val initialPos: Int) {
     var pos = initialPos;
-    var importantLocationMeets = 0;
+    var importantLocationMeets = 0
+    var importantLocationPasses = 0
 
     fun rotate(rotation: String) {
         val direction = rotation.first()
         val amount = rotation.drop(1).toInt()
+        val prevPos = pos
         when (direction) {
             'L' -> pos = (pos-amount).mod(size)
             'R' -> pos = (pos+amount).mod(size)
+        }
+        if (pos == importantLocation) {
+            importantLocationMeets++
+        }
+        if (prevPos != 0) {
+            importantLocationPasses += when (direction) {
+                'L' -> ((size - prevPos) + amount) / size
+                'R' -> (prevPos + amount) / size
+                else -> 0
+            }
+        }
+
+//        println("${rotation} (pos: $pos): meets: $importantLocationMeets; passes: $importantLocationPasses")
+    }
+
+    fun rotateSlowly(rotation: String) {
+        val direction = rotation.first()
+        val amount = rotation.drop(1).toInt()
+        for (i in 0..amount-1){
+            when(direction) {
+                'L' -> pos--
+                'R' -> pos++
+            }
+            pos = (pos).mod(size)
+            if (pos == importantLocation) {
+                importantLocationPasses++
+            }
         }
         if (pos == importantLocation) {
             importantLocationMeets++
